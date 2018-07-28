a21db184923a09b13fb80c6d8efba4f671974507
Patrick Sollars <pjsollars@gmail.com>
2018-07-28 14:10:54 -0400

    add event handlers

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,22 +1,47 @@
-import React, { Component } from 'react';
-import './../styles/App.css';
+import React, { Component } from "react";
+import "./../styles/App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <label>Qty:
-          <input />
+        <label>
+          Qty:
+          <input onChange={this.handleQty}/>
         </label>
-        <label>Desc:
-          <input />
+        <label>
+          Desc:
+          <input onChange={this.handleDesc}/>
         </label>
-        <button> 
+        <button onClick={this.handleSave}>
           Save this
         </button>
       </div>
     );
   }
+
+  handleQty = (e) => {
+    this.setState({
+      qty: e.target.value
+    });
+  }
+
+  handleDesc = (e) => {
+    this.setState({
+      desc: e.target.value
+    });
+  }
+
+  handleSave = () => {
+    alert("yo");
+  };
 }
 
 export default App;
