46f81091f460b1202b000f2e21bcc541539eac3e
Patrick Sollars <pjsollars@gmail.com>
2019-02-11 20:05:55 -0500

    Comment out test component in routes

--- client/src/router/routes.js
+++ client/src/router/routes.js
@@ -1,9 +1,9 @@
 import Recipe from "../components/recipe/Recipe";
 import Recipes from "../components/recipe/Recipes";
-import Test from "../components/Test";
+// import Test from "../components/Test";
 
 export default [
   { path: "/recipe/:id", component: Recipe },
-  { path: "/recipes", component: Recipes },
-  { path: "/test", component: Test }
+  { path: "/recipes", component: Recipes }
+  // { path: "/test", component: Test }
 ];
