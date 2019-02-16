4efd197d596f6afe60e9d69abf80a6400112bd33
Patrick Sollars <pjsollars@gmail.com>
2019-02-16 16:22:43 -0500

    Remove experimental files

--- client/src/router/routes.js
+++ client/src/router/routes.js
@@ -1,9 +1,7 @@
 import Recipe from "../components/recipe/Recipe";
 import Recipes from "../components/recipe/Recipes";
-// import Test from "../components/Test";
 
 export default [
   { path: "/recipe/:id", component: Recipe },
   { path: "/recipes", component: Recipes }
-  // { path: "/test", component: Test }
 ];
